54d205116bc91c867eb63f213a60432c97592b5f
ShubhamMadheshiya02 <156683219+ShubhamMadheshiya02@users.noreply.github.com>
2026-07-23 04:52:49 +0530

    Update REGISTER.v

--- RTL/REGISTER.v
+++ RTL/REGISTER.v
@@ -1,115 +1,113 @@
-module FSM_CONTROLLER(
+//**
+
+
+module REGISTERs(
     input clk,
     input resetn,
-    input pkt_valid,
+    input [7:0]data_in,
+    input detect_add,
     input fifo_full,
-    input fifo_empty0,fifo_empty1,fifo_empty2,
-    input soft_reset0,soft_reset1,soft_reset2,
-    input parity_done,
-    input low_packet_valid,
-    input [1:0]fifo_add,
-    output write_en,
-    output detect_add,
-    output ld_state,
-    output laf_state,
-    output lfd_state,
-    output full_state,
-    output parity_check_state,
-    output busy,
-    output load_parity_state
-    
-
+    input full_state,
+    input laf_state,
+    input ld_state,
+    input lfd_state,
+    input pkt_valid,
+    input parity_check_state,
+    input load_parity_state,
+    output logic [7:0]data_out,
+    output logic err,
+    output logic low_packet_valid,
+    output logic parity_done
     );
     
-    typedef enum bit[2:0]{DETECT_ADD,LOAD_ADDRESS,LOAD_DATA,FIFO_FULL,WAIT_TILL_EMPTY,
-                            LOAD_AFTER_FULL,CHECK_PARITY_ERROR,LOAD_PARITY} fsm_states;
-     fsm_states ps,ns;
-     logic [1:0] stored_addr;
-     
-     always @(posedge clk)begin
+    reg [7:0]header;
+    reg [7:0]int_reg;
+    reg [7:0]int_parity,ext_parity;
+    reg [7:0] data_in_temp;
+ 
+    
+    always @(posedge clk) begin
         if(!resetn)begin
-            ps <= DETECT_ADD;
+             header <= 8'hzz;
+             int_reg <= 0;
+             data_out <= 0;
+             data_in_temp <= 8'hzz;
         end
+        else begin 
+            if(detect_add && pkt_valid && data_in[1:0]!= 2'b11)begin
+                header <= data_in;
+                //data_in_temp <= data_in;
+            end
+             if(lfd_state)begin
+                data_out <= header;
+                //data_in_temp = data_in;
+                end
+            else if(ld_state && !fifo_full)begin
+               // data_in_temp <= data_in;
+                data_out <= data_in;end
+            else if(full_state)
+                int_reg <= data_in;
+            else if(laf_state)
+                data_out <= int_reg;
+        end
+    end
+    
+    always @(posedge clk)begin
+        if(!resetn)begin
+            int_parity <= 0;
+        end
+        else if(detect_add)begin
+            int_parity <= 0;
+        end
+        else if(lfd_state) begin
+            int_parity <= int_parity^data_in;
+        end
+        else if(ld_state && !full_state && pkt_valid) begin
+            int_parity <= int_parity^data_in;
+        end 
+        else 
+            int_parity <= int_parity;    
+    end
+    
+    always @(posedge clk)begin
+        if(!resetn)begin
+            low_packet_valid <= 0;
+            ext_parity <= 0;   
+        end
+        else if(load_parity_state)begin
+            ext_parity <= data_in;
+        end
+    end
+    
+    always @(posedge clk)begin
+        if(!resetn)begin
+            err <= 0;
+            parity_done <= 0;
+        end
+        else if(parity_check_state)
+        begin
+            parity_done <= 1;
+            if(int_parity == ext_parity)
+                err <= 0;
+            else
+                err <= 1;
+         end
+         else begin
+            err <= 0;
+            parity_done <= 0;
+         end
+                 
+    end
+    
+    always @(posedge clk)begin
+        if(!resetn)begin
+            low_packet_valid <= 0;    
+        end
+        else if(ld_state  && !pkt_valid)
+            low_packet_valid <= 1;
         else
-            ps <= ns;
-     
-     end
-     
-     always @(*)begin
-        ns = ps; 
-        case(ps)
-        DETECT_ADD : begin
-                        if((pkt_valid && (fifo_add==0) && fifo_empty0)||
-                           (pkt_valid && (fifo_add==1) && fifo_empty1)||
-                           (pkt_valid && (fifo_add==2) && fifo_empty2))begin
-                            ns = LOAD_ADDRESS;
-                            stored_addr = fifo_add; 
-                           end
-                        else if((pkt_valid && (fifo_add==0) && (~fifo_empty0))||
-                                (pkt_valid && (fifo_add==1) && (~fifo_empty1))||
-                                (pkt_valid && (fifo_add==2) && (~fifo_empty2)))
-                            ns = WAIT_TILL_EMPTY;
-                        else
-                            ns = DETECT_ADD;     
-                     end
-         LOAD_ADDRESS : begin
-                            if(pkt_valid)
-                                ns = LOAD_DATA;
-                            else
-                                ns = DETECT_ADD;    
-                        end
-         LOAD_DATA : begin
-                        if(fifo_full)begin
-                            ns = FIFO_FULL;
-                        end
-                        else if(!pkt_valid)
-                            ns = LOAD_PARITY;    
-                        else
-                            ns = LOAD_DATA;
-                    end 
-         LOAD_PARITY : begin
-                            ns = CHECK_PARITY_ERROR;            
-                       end
-         CHECK_PARITY_ERROR : begin
-                                if(fifo_full)
-			      	 			ns =FIFO_FULL;
-			    			else if(parity_done)
-			         			ns = DETECT_ADD;     
-                              end 
-         WAIT_TILL_EMPTY : begin
-                                if((stored_addr == 0 && fifo_empty0) || (stored_addr == 1 && fifo_empty1) || (stored_addr == 2 && fifo_empty2)) begin
-                                    ns = LOAD_ADDRESS;
-                                end
-                                else
-                                    ns = WAIT_TILL_EMPTY;
-                           end
-         FIFO_FULL : begin
-                            if(!fifo_full)
-                                ns = LOAD_AFTER_FULL;
-                            else
-                                ns = FIFO_FULL;     
-                     end
-         LOAD_AFTER_FULL : begin
-                                if(parity_done)
-                                    ns = DETECT_ADD;
-                                else if(!low_packet_valid)
-                                    ns = LOAD_DATA;
-                                else if(low_packet_valid)
-                                    ns = LOAD_PARITY;
-                                
-                            end
-         endcase
-     end
-  
-    assign detect_add  = (ps == DETECT_ADD);
-    assign lfd_state   = (ps == LOAD_ADDRESS);
-    assign ld_state    = (ps == LOAD_DATA);
-    assign full_state  = (ps == FIFO_FULL);
-    assign laf_state   = (ps == LOAD_AFTER_FULL);
-    assign parity_check_state = (ps == CHECK_PARITY_ERROR);
-    assign load_parity_state = (ps == LOAD_PARITY);
-
-    assign write_en    = /*(ps == LOAD_ADDRESS) ||*/ (ps == LOAD_DATA) || (ps == LOAD_PARITY) || (ps == LOAD_AFTER_FULL);
-    assign busy        = (ps != DETECT_ADD);
-     
+            low_packet_valid <= 0;    
+    end
+       
 endmodule
+/**/
