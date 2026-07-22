7c88a037ebd2775b6b6f82383fbcae617878f228
ShubhamMadheshiya02 <156683219+ShubhamMadheshiya02@users.noreply.github.com>
2026-07-23 04:55:26 +0530

    Add testbench for FIFO module

---
+++ Test_bench/FIFO_TB.v
@@ -0,0 +1,69 @@
+
+
+module tb_FIFO;
+
+
+    // Testbench signals
+    logic           clk;
+    logic           resetn;
+    logic   [7:0]   data_in;
+    logic           read_en;
+    logic           soft_reset;
+    logic           lfd_state;
+    logic           write_en;
+
+   
+    wire    [7:0]   data_out;
+    wire            full;
+    wire            empty;
+    
+    
+    FIFO dut (
+        .clk(clk),
+        .resetn(resetn),
+        .data_in(data_in),
+        .read_en(read_en),
+        .soft_reset(soft_reset),
+        .lfd_state(lfd_state),
+        .write_en(write_en),
+        .data_out(data_out),
+        .full(full),
+        .empty(empty)
+    );
+    
+    initial begin
+     clk = 0 ;
+     forever #10 clk = ~clk;
+    
+    end
+    initial begin
+        resetn = 1'b0; 
+        write_en = 0;
+        read_en = 0;
+        data_in = 8'd0;
+        lfd_state = 0;
+    end
+    
+    initial begin
+        #20;
+        resetn = 1'b1;
+        for (int i = 0; i < 16; i++) begin
+            @(posedge clk);
+            write_en = 1;
+            data_in = i;
+            $display("Time=%0t | Write Enabled | data_in=%d | full=%b, empty=%b, counter=%d", $time, data_in, full, empty, dut.counter);
+        end
+        @(posedge clk);
+        write_en = 0; 
+        for (int i = 0; i < 16; i++) begin
+            @(posedge clk);
+            read_en = 1;
+            $display("Time=%0t | Read Enabled  | data_out=%d (Expected %d) | full=%b, empty=%b, counter=%d", $time, data_out, i, full, empty, dut.counter);
+        end
+        @(posedge clk);
+        read_en = 0; // Stop reading
+
+        $finish;
+    end
+    
+endmodule
